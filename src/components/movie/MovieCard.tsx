478a21d1d64090e1f47b1cd10851c37432e08d8a
Damir Lončarić <damir.loncaric987@gmail.com>
2025-04-26 13:32:44 +0200

    shrink MovieCard size

--- src/components/movie/MovieCard.tsx
+++ src/components/movie/MovieCard.tsx
@@ -12,26 +12,29 @@ interface MovieCardProps {
 
 export default function MovieCard({ movie }: MovieCardProps) {
   return (
-    <Card className="relative w-48 overflow-hidden bg-neutral-900 hover:border-yellow-400 hover:shadow-yellow-400 transition border border-neutral-800 shadow-md">
+    <Card
+      className="relative w-36 overflow-hidden bg-neutral-900 hover:border-yellow-400
+       hover:shadow-yellow-400 transition border border-neutral-800 shadow-md"
+    >
       {/* Favorite Button */}
       <div className="absolute top-2 right-2 z-10">
         <FavoriteButton movie={movie} />
       </div>
 
       {/* Poster */}
-      <div className="w-full h-72 relative">
+      <div className="w-full h-48 relative">
         <Image
           src={getImageUrl(movie.poster_path)}
           alt={movie.title}
           fill
           className="object-cover"
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 33vw, 20vw"
+          sizes="(max-width: 768px) 50vw, (max-width: 1200px) 25vw, 15vw"
           priority
         />
       </div>
 
       {/* Title */}
-      <CardContent className="p-3">
+      <CardContent className="p-1">
         <h2 className="text-sm font-semibold text-white">{movie.title}</h2>
       </CardContent>
     </Card>
