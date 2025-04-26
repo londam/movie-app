b99ea9e6148f10e79f6296bf3ab5868b41bd3b1c
Damir Lončarić <damir.loncaric987@gmail.com>
2025-04-26 15:54:19 +0200

    Extracted both TMDB types into tmdb type file

--- src/types/tmdb.ts
+++ src/types/tmdb.ts
@@ -2,6 +2,15 @@ export interface TMDBMovie {
   id: number;
   title: string;
   poster_path: string;
+  overview: string;
+  vote_average: number;
+  genre_ids: number[];
   release_date: string;
-  // other fields later like overview, release_date, etc.
+}
+
+export interface TMDBMovieListResponse {
+  page: number;
+  results: TMDBMovie[];
+  total_pages: number;
+  total_results: number;
 }
