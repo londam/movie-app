bdc66c2a60ca4459eb5f18f815d52168aeeaee2e
Damir Lončarić <damir.loncaric987@gmail.com>
2025-04-26 15:55:14 +0200

    feat: added getPopularMoviesByGenre into movie-service.ts

--- src/lib/movie-service.ts
+++ src/lib/movie-service.ts
@@ -1,9 +1,5 @@
-import { TMDBMovie } from "@/types/tmdb";
+import { TMDBMovieListResponse } from "@/types/tmdb";
 import { fetchFromTMDB } from "./api";
-
-interface TMDBMovieListResponse {
-  results: TMDBMovie[];
-}
 
 export async function getPopularMovies() {
   const data = await fetchFromTMDB<TMDBMovieListResponse>("/movie/popular", {
@@ -20,5 +16,13 @@ export async function getNowPlayingMovies() {
     page: "1",
   });
 
+  return data.results;
+}
+
+export async function getPopularMoviesByGenre(genreId: number) {
+  const data = await fetchFromTMDB<TMDBMovieListResponse>("/discover/movie", {
+    with_genres: genreId.toString(),
+    sort_by: "popularity.desc",
+  });
   return data.results;
 }
