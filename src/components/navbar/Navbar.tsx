8b40156aedc0bf9244f3e8e24034a05b129f0b09
Damir Lončarić <damir.loncaric987@gmail.com>
2025-04-28 15:00:36 +0200

    Added MostWatched link to navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -9,15 +9,21 @@ const Navbar = () => {
     <header className="w-full px-4 py-3 border-b border-neutral-800 bg-black sticky top-0 z-50">
       <div className="max-w-7xl mx-auto flex items-center justify-between">
         {/* Logo / App Title */}
-        <Link
-          href="/"
-          className="text-2xl font-bold text-yellow-400 hover:text-yellow-200 transition"
-        >
-          🎬 Movie App
-        </Link>
-        <Link href="/most-watched">
-          <h2>Most Watched</h2>
-        </Link>
+        <div className="flex items-center gap-6">
+          <Link
+            href="/"
+            className="text-2xl font-bold text-yellow-400 hover:text-yellow-200 transition"
+          >
+            🎬 Movie App
+          </Link>
+
+          <Link
+            href="/most-watched"
+            className="text-sm font-semibold text-white hover:text-yellow-400 transition"
+          >
+            Most Watched
+          </Link>
+        </div>
         {/* Right Side: Search + Favorites */}
         <div className="flex items-center gap-4">
           <SearchBar />
